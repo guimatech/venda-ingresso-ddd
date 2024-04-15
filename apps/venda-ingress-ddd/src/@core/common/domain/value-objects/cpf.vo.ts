f4c24d252fb7d4dfca4716f4cf21e43b7bab4454
Lucas dos Santos Guimarães <guimaraes-lucas@outlook.com.br>
2024-04-15 11:24:08 -0300

    fix: cpf validate

--- apps/venda-ingress-ddd/src/@core/common/domain/value-objects/cpf.vo.ts
+++ apps/venda-ingress-ddd/src/@core/common/domain/value-objects/cpf.vo.ts
@@ -1,28 +1,27 @@
 import { ValueObject } from './value-object';
 
-class Cpf extends ValueObject<string> {
+export class Cpf extends ValueObject<string> {
   constructor(value: string) {
     super(value.replace(/\D/g, ''));
     this.validate();
   }
 
   private validate(): void {
-    if (this.value.length !== 11) {
+    if (this.value.length != 11) {
       throw new InvalidCpfError(
-        'CPF must have 11 digits, but has ' + this.value.length + ' digits'
+        'CPF must have 11 digits, but has ' + this.value.length + ' digits',
       );
     }
 
-    const allDigitsAreEqual = /^\d{1}(\d)\1{10}$/.test(this.value);
-    if (allDigitsAreEqual) {
+    const allDigitsEquals = /^\d{1}(\d)\1{10}$/.test(this.value);
+    if (allDigitsEquals) {
       throw new InvalidCpfError('CPF must have at least two different digits');
     }
 
     let sum = 0;
     for (let i = 0; i < 9; i++) {
-      sum += parseInt(this.value[i]) * (10 - i);
+      sum += parseInt(this.value.charAt(i)) * (10 - i);
     }
-
     let firstDigit = 11 - (sum % 11);
     if (firstDigit > 9) {
       firstDigit = 0;
@@ -30,19 +29,18 @@ class Cpf extends ValueObject<string> {
 
     sum = 0;
     for (let i = 0; i < 10; i++) {
-      sum += parseInt(this.value.charAt[i]) * (11 - i);
+      sum += parseInt(this.value.charAt(i)) * (11 - i);
     }
-
     let secondDigit = 11 - (sum % 11);
     if (secondDigit > 9) {
       secondDigit = 0;
     }
 
     if (
-      parseInt(this.value.charAt[9]) !== firstDigit ||
-      parseInt(this.value.charAt[10]) !== secondDigit
+      firstDigit !== parseInt(this.value.charAt(9)) ||
+      secondDigit !== parseInt(this.value.charAt(10))
     ) {
-      throw new InvalidCpfError('Invalid CPF');
+      throw new InvalidCpfError('CPF is invalid');
     }
   }
 }
